898c0df24fbcf6584c63a02dfe31bd1714a52b99
beror <beror>
2023-09-12 16:49:47 +0300

    refactor: move state button triplets into their own component
    Move the button triplets that show re-render and derived-state efficiency into their own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
-
-import StateButton from './components/StateButton';
-import { useGlobalState as useContextBasedGlobalState } from './stores/contextBased/store';
-import { useGlobalState as useClosureBasedGlobalState } from './stateManagers/closureBased/hooks';
-import closureBasedStore, { COUNTER_ACTIONS as closureBasedCounterActions } from './stores/closureBased/store';
-import react18BasedStore, { COUNTER_ACTIONS as react18BasedCounterActions } from './stores/react18Based/store';
+import { useState } from 'react';
 
 import closureBasedCreateStore from './assets/closureBasedCreateStore.png';
 import closureBasedHook from './assets/closureBasedHook.png';
@@ -17,84 +11,9 @@ import react18BasedCreateStore from './assets/react18BasedCreateStore.png';
 import react18BasedStoreCreation from './assets/react18BasedStoreCreation.png';
 import react18BasedUsage from './assets/react18BasedUsage.png';
 import githubMark from './assets/github-mark.svg';
+import StateShowcase from './components/StateShowcase';
 
 function App() {
-  const {
-    counter1: counter1ClosureBased,
-    counter2: counter2ClosureBased
-  } = useClosureBasedGlobalState(closureBasedStore.getState);
-  const [
-    derivedStateFromClosureBasedStates,
-    setDerivedStateFromClosureBasedStates
-  ] = useState(counter1ClosureBased + counter2ClosureBased);
-
-  useEffect(() => {
-    setDerivedStateFromClosureBasedStates(counter1ClosureBased + counter2ClosureBased);
-  }, [counter1ClosureBased, counter2ClosureBased]);
-
-  const counter1ClosureBasedButtonOnClick = useCallback(
-    () => closureBasedStore.dispatch({ type: closureBasedCounterActions.INCREMENT_COUNTER_1 }),
-    []
-  );
-
-  const counter2ClosureBasedButtonOnClick = useCallback(
-    () => closureBasedStore.dispatch({ type: closureBasedCounterActions.INCREMENT_COUNTER_1 }),
-    []
-  );
-
-
-  const {
-    counter1: counter1React18Based,
-    counter2: counter2React18Based
-  } = react18BasedStore.useGlobalState();
-  const [
-    derivedStateFromReact18BasedStates,
-    setDerivedStateFromReact18BasedStates
-  ] = useState(counter1React18Based + counter2React18Based);
-
-  useEffect(() => {
-    setDerivedStateFromReact18BasedStates(counter1React18Based + counter2React18Based);
-  }, [counter1React18Based, counter2React18Based]);
-
-  const counter1React18BasedButtonOnClick = useCallback(
-    () => react18BasedStore.dispatch({ type: react18BasedCounterActions.INCREMENT_COUNTER_1 }),
-    []
-  );
-  const counter2React18BasedButtonOnClick = useCallback(
-    () => react18BasedStore.dispatch({ type: react18BasedCounterActions.INCREMENT_COUNTER_2 }),
-    []
-  );
-
-
-  const {
-    globalState: contextBasedGlobalState,
-    setGlobalState: setContextBasedGlobalState
-  } = useContextBasedGlobalState();
-  const [
-    derivedStateFromContextBasedStates,
-    setDerivedStateFromContextBasedStates
-  ] = useState(contextBasedGlobalState.counter1 + contextBasedGlobalState.counter2);
-
-  useEffect(() => {
-    setDerivedStateFromContextBasedStates(contextBasedGlobalState.counter1 + contextBasedGlobalState.counter2);
-  }, [contextBasedGlobalState.counter1, contextBasedGlobalState.counter2]);
-
-  const counter1ContextBasedButtonOnClick = useCallback(
-    () => setContextBasedGlobalState((state) => ({
-      ...state,
-      counter1: state.counter1 + 1
-    })),
-    []
-  );
-  const counter2ContextBasedButtonOnClick = useCallback(
-    () => setContextBasedGlobalState((state) => ({
-      ...state,
-      counter2: state.counter2 + 1
-    })),
-    []
-  );
-
-
   const [ isSameSignHovered, setIsSameSignHovered ] = useState(false);
 
   return (
@@ -122,15 +41,7 @@ function App() {
           </div>
           <img src={closureBasedHook} alt='Showcase of the hook in closure-based state manager' />
           <img src={closureBasedUsage} alt='Showcase of usage of closure-based state manager' />
-          <StateButton onClick={counter1ClosureBasedButtonOnClick}>
-            {`counter 1: ${counter1ClosureBased}`}
-          </StateButton>
-          <StateButton onClick={counter2ClosureBasedButtonOnClick}>
-            {`counter 2: ${counter2ClosureBased}`}
-          </StateButton>
-          <StateButton>
-            {`Derived state (sum): ${derivedStateFromClosureBasedStates}`}
-          </StateButton>
+          <StateShowcase.ClosureBased />
         </div>
 
         <div className='manager-presentation-box'>
@@ -147,15 +58,7 @@ function App() {
             </div>
           </div>
           <img src={react18BasedUsage} alt='Showcase of usage of React-18-based state manager' />
-          <StateButton onClick={counter1React18BasedButtonOnClick}>
-            {`counter 1: ${counter1React18Based}`}
-          </StateButton>
-          <StateButton onClick={counter2React18BasedButtonOnClick}>
-            {`counter 2: ${counter2React18Based}`}
-          </StateButton>
-          <StateButton>
-            {`Derived state (sum): ${derivedStateFromReact18BasedStates}`}
-          </StateButton>
+          <StateShowcase.React18Based />
         </div>
 
         <div className='manager-presentation-box'>
@@ -164,15 +67,7 @@ function App() {
           <img src={contextBasedStoreCreation} alt='Showcase of store creation in Context-based state manager' />
           <img src={contextBasedProvidingState} alt='Showcase of providing the state of Context-based state manager to the application' />
           <img src={contextBasedUsage} alt='Showcase of usage of context-based state manager' />
-          <StateButton onClick={counter1ContextBasedButtonOnClick}>
-            {`counter 1: ${contextBasedGlobalState.counter1}`}
-          </StateButton>
-          <StateButton onClick={counter2ContextBasedButtonOnClick}>
-            {`counter 2: ${contextBasedGlobalState.counter2}`}
-          </StateButton>
-          <StateButton>
-            {`Derived state (sum): ${derivedStateFromContextBasedStates}`}
-          </StateButton>
+          <StateShowcase.ContextBased />
         </div>
       </main>
     </>
---
+++ src/components/StateShowcase.tsx
@@ -0,0 +1,119 @@
+import { useState, useEffect, useCallback } from 'react';
+
+import StateButton from './StateButton';
+import { useGlobalState as useContextBasedGlobalState } from '../stores/contextBased/store';
+import { useGlobalState as useClosureBasedGlobalState } from '../stateManagers/closureBased/hooks';
+import closureBasedStore, { COUNTER_ACTIONS as closureBasedCounterActions } from '../stores/closureBased/store';
+import react18BasedStore, { COUNTER_ACTIONS as react18BasedCounterActions } from '../stores/react18Based/store';
+
+const ClosureBased = () => {
+  const { counter1, counter2 } = useClosureBasedGlobalState(closureBasedStore.getState);
+  const [ derivedState, setDerivedState ] = useState(counter1 + counter2);
+
+  useEffect(() => {
+    setDerivedState(counter1 + counter2);
+  }, [counter1, counter2]);
+
+  const counter1ButtonOnClick = useCallback(
+    () => closureBasedStore.dispatch({ type: closureBasedCounterActions.INCREMENT_COUNTER_1 }),
+    []
+  );
+
+  const counter2ButtonOnClick = useCallback(
+    () => closureBasedStore.dispatch({ type: closureBasedCounterActions.INCREMENT_COUNTER_1 }),
+    []
+  );
+
+  return (
+    <>
+      <StateButton onClick={counter1ButtonOnClick}>
+        {`counter 1: ${counter1}`}
+      </StateButton>
+      <StateButton onClick={counter2ButtonOnClick}>
+        {`counter 2: ${counter2}`}
+      </StateButton>
+      <StateButton>
+        {`Derived state (sum): ${derivedState}`}
+      </StateButton>
+    </>
+  );
+};
+
+const React18Based = () => {
+  const { counter1, counter2 } = react18BasedStore.useGlobalState();
+  const [ derivedState, setDerivedState ] = useState(counter1 + counter2);
+
+  useEffect(() => {
+    setDerivedState(counter1 + counter2);
+  }, [counter1, counter2]);
+
+  const counter1ButtonOnClick = useCallback(
+    () => react18BasedStore.dispatch({ type: react18BasedCounterActions.INCREMENT_COUNTER_1 }),
+    []
+  );
+  const counter2ButtonOnClick = useCallback(
+    () => react18BasedStore.dispatch({ type: react18BasedCounterActions.INCREMENT_COUNTER_2 }),
+    []
+  );
+
+  return (
+    <>
+      <StateButton onClick={counter1ButtonOnClick}>
+        {`counter 1: ${counter1}`}
+      </StateButton>
+      <StateButton onClick={counter2ButtonOnClick}>
+        {`counter 2: ${counter2}`}
+      </StateButton>
+      <StateButton>
+        {`Derived state (sum): ${derivedState}`}
+      </StateButton>
+    </>
+  );
+};
+
+const ContextBased = () => {
+  const { globalState, setGlobalState } = useContextBasedGlobalState();
+  const [
+    derivedState,
+    setDerivedState
+  ] = useState(globalState.counter1 + globalState.counter2);
+
+  useEffect(() => {
+    setDerivedState(globalState.counter1 + globalState.counter2);
+  }, [globalState.counter1, globalState.counter2]);
+
+  const counter1ButtonOnClick = useCallback(
+    () => setGlobalState((state) => ({
+      ...state,
+      counter1: state.counter1 + 1
+    })),
+    []
+  );
+  const counter2ButtonOnClick = useCallback(
+    () => setGlobalState((state) => ({
+      ...state,
+      counter2: state.counter2 + 1
+    })),
+    []
+  );
+
+  return (
+    <>
+      <StateButton onClick={counter1ButtonOnClick}>
+        {`counter 1: ${globalState.counter1}`}
+      </StateButton>
+      <StateButton onClick={counter2ButtonOnClick}>
+        {`counter 2: ${globalState.counter2}`}
+      </StateButton>
+      <StateButton>
+        {`Derived state (sum): ${derivedState}`}
+      </StateButton>
+    </>
+  );
+};
+
+export default {
+  ClosureBased,
+  React18Based,
+  ContextBased
+};
