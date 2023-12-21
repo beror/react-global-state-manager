b3cf622928f316ef3827d9cd78409bd850e8dbb9
beror <34488024+beror@users.noreply.github.com>
2023-12-21 09:16:07 +0200

    docs: add comment pointing out potential issue

--- src/stateManagers/closureBased/hooks.ts
+++ src/stateManagers/closureBased/hooks.ts
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import store from '../../stores/closureBased/store';
 
 export const useGlobalState = <State>(getState: () => State) => {
-  const [ state, setState ] = useState(getState);
+  const [ state, setState ] = useState(getState); // TODO: shouldn't the initial value be getState(), not getState?
 
   useEffect(() => store.subscribe(() => setState(getState())), [getState]);
 
